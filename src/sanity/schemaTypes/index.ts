d8bea53d2c1b92ff84d65470b0fe169b3c06370b
Frances Loggins <prettyinpurple2021@gmail.com>
2026-03-02 08:16:51 -0500

    fix: commit schemaTypes export in sanity schema index

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,6 +1,3 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import { postType } from './postType'
+import {postType} from './postType'
 
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [postType],
-}
+export const schemaTypes = [postType]
